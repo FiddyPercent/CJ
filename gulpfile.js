01576274d18d1f1c83255fcd38b81acd1ed5c53a
Ashly Berry <ashlyberry@Ymail.com>
2018-04-02 02:02:43 -0700

    more sub files

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+// gulp.task('test', function() {
+// 	console.log('this test proves true');
+// });
+
+gulp.task('sass', function() {
+	return gulp.src('app/scss/styles.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('app/css'))
+});
