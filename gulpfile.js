1bfa54d2cb7b9f0a1b03f35eefbfa7ae40b9e7d2
Ashly Berry <ashlyberry@Ymail.com>
2018-04-02 02:36:02 -0700

    fixed sass dependency to gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-// gulp.task('test', function() {
-// 	console.log('this test proves true');
-// });
+gulp.task('test', function() {
+	console.log('this test proves true');
+});
 
 gulp.task('sass', function() {
 	return gulp.src('app/scss/styles.scss')
